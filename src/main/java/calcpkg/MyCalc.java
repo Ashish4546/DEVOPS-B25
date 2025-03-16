14fec267e795b6db6d3ebd44005c8283cba063b0
ashishdhenge45 <98308174+ashishdhenge45@users.noreply.github.com>
2025-03-16 06:49:50 -0400

    div () is added

--- src/main/java/calcpkg/MyCalc.java
+++ src/main/java/calcpkg/MyCalc.java
@@ -17,11 +17,17 @@ public class MyCalc {
 		return a*b;
 	}
 	
+	public int div (int a, int b)
+	{
+		return a/b;
+	}
+	
 	public static void main(String[] args) {
 		MyCalc ob = new MyCalc() ;
 		System.out.println("Sum is: "+ob.sum(20, 10)) ;
 		System.out.println("Diff is: "+ob.diff(20, 10));
 		System.out.println("Mul is: "+ob.mul(20, 10));
+		System.out.println("Div is: "+ob.div(20, 10));
 	}
 
 }
